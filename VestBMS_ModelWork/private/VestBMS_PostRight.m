e357892825119cf5fbd42d603d2e4be7c9e5f1e4
Luigi Acerbi <luigi.acerbi@nyu.edu>
2016-08-02 07:22:04 -0400

    improved speed

--- VestBMS_ModelWork/private/VestBMS_PostRight.m
+++ VestBMS_ModelWork/private/VestBMS_PostRight.m
@@ -1,10 +1,13 @@
 function postright = VestBMS_PostRight(postpdf)
 %VESTBMD_POSTRIGHT Posterior probability of perceiving rightward motion.
 
-postpdf = postpdf + realmin;    % Avoid zero probability
-idx0deg = (size(postpdf,1)+1)/2; % Index of 0 deg
-postleft = qtrapz(postpdf(1:idx0deg,:,:),1);
-postright = qtrapz(postpdf(idx0deg:end,:,:),1);
+n = size(postpdf,1);
+pmin = realmin*n/2;
+
+% postpdf = postpdf + realmin;    % Avoid zero probability
+idx0deg = (n+1)/2; % Index of 0 deg
+postleft = qtrapz(postpdf(1:idx0deg,:,:),1) + pmin;
+postright = qtrapz(postpdf(idx0deg:end,:,:),1) + pmin;
 postright = postright./(postright + postleft);
 
 end

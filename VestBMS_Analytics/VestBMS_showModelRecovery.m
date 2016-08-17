aca7d63a5bf6aab07af277a817ae6030c0905e75
Luigi Acerbi <luigi.acerbi@nyu.edu>
2016-08-16 22:21:38 -0400

    model recovery

---
+++ VestBMS_Analytics/VestBMS_showModelRecovery.m
@@ -0,0 +1,27 @@
+function [ctab,bias,se,mtab,stab] = VestBMS_showModelRecovery(task,mbag)
+%VESTBMS_SHOWMODELRECOVERY Show model recovery results.
+
+if nargin < 2; mbag = []; end
+
+% Datasets
+Nsubjs = 11;
+N = 10;
+dataids = [];
+for i = 1:4
+    temp = bsxfun(@plus, (1:5)', ((1:Nsubjs)-1)*N) + (i-1)*(N*Nsubjs);
+    dataids = [dataids; temp(:)];
+end
+
+switch lower(task(1))
+    case 'u'; fitname = 'VestBMS_21001'; dataset = 'VestBMS_fakedata_unity';
+    case 'l'; fitname = 'VestBMS_20001'; dataset = 'VestBMS_fakedata_bimloc';
+    otherwise
+        error('Unknown task. Specify either (U)nity judgment or bimodal (L)ocalization.');        
+end
+load(dataset);
+if isempty(mbag)
+    load(['.' filesep 'modelrecovery' filesep fitname]);
+end
+[ctab,bias,se,mtab,stab] = ModelWork_plotModelRecovery(data(dataids),mbag,modelsummary);
+
+end

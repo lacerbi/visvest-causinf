e0f87e223d9481db39300f8cf9617a1b97837361
Luigi Acerbi <luigi.acerbi@nyu.edu>
2016-08-18 07:55:00 -0700

    added model name for correlated prior

--- VestBMS_ModelWork/VestBMS_getModelName.m
+++ VestBMS_ModelWork/VestBMS_getModelName.m
@@ -14,7 +14,13 @@ end
 if model(11) == 3
     string = [string 'P'];
 end
-        
+
+switch model(9)
+    case 1
+    case 2; string = [string 'u'];
+    case 3; string = [string 'U'];
+end
+
 switch model(1)
     case 1; string = [string '-C'];
 end

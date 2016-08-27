d833f6a80150a1d1dbdec47c9e485f17fa257c46
Luigi Acerbi <luigi.acerbi@nyu.edu>
2016-08-27 09:30:56 -0400

    added correlated prior only (constant)

--- VestBMS_ModelWork/VestBMS_batchrunInit.m
+++ VestBMS_ModelWork/VestBMS_batchrunInit.m
@@ -445,6 +445,23 @@ switch type
        models_extra(:,9) = 5;  % Free delta prior
        models = [models; models_extra];    
     
+   case {405} % Bisensory Bayesian models with *constant* noise, deterministic decision making and lapse -- CORRELATED DISCRETE PRIORS ONLY
+        % THIS MODEL BELONGS TO THE FINAL MODEL SET
+       
+       [options,models,groupcnd] = VestBMS(options,2,0);
+       models = models(1,:);
+       models(:,[1 2]) = 1; % Constant noise
+       models(:,11) = 1;    % BDT
+       options.jobname = 'vest_bayes_const_disconly';
+       models(:,8) = 2;     % Fixed-mean prior
+       models(:,13) = 2;    % Lapse
+       models(:,15) = 7;    % Correlated prior only
+       dataids(:,2) = setflag(dataids(:,2), 4);     % No categorical trials
+       models(:,9) = 4; % Uniform discrete prior on DELTA
+       models_extra = models;
+       models_extra(:,9) = 5;  % Free discrete Gaussian prior on DELTA
+       models = [models; models_extra];
+       
     
    case {411} % Bisensory Bayesian models with *sinusoidal* noise, deterministic decision making and lapse -- CORRELATED DISCRETE PRIORS
         % THIS MODEL BELONGS TO THE FINAL MODEL SET
